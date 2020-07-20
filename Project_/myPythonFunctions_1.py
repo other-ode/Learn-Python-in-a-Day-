56b74acfe004eb54e11091de7a0be214a92b116c
David Odebade <other.ode@gmail.com>
2020-07-20 18:37:11 +0100

    Sample Project Solutions

--- Project_/myPythonFunctions_1.py
+++ Project_/myPythonFunctions_1.py
@@ -4,13 +4,20 @@
 import random as r
 import os
 
+fileName = 'C:\\Users\\dev-research\\Documents\\Projects\\Applications\\Learn-Python-in-a-Day-\\Project_\\userScores1.txt'
+
 # Exercise 2: Getting the User’s Score
 def getUserPoint(userName):
-    f = open('C:\\Users\\dev-research\\Documents\\Projects\\Applications\\Learn-Python-in-a-Day-\\Project_\\userScores.txt', 'r')
-    for line in f:
-        name, score = line.split(",")
-        if (name == userName):
-            return score
-        return '-1'
+    try:
+        f = open(fileName, 'r')
+        for line in f:
+            name, score = line.split(",")
+            if (name == userName):
+                return score
+            return '-1'
+            f.close()
+    except IOError as e:
+        print('File not found ', e)
+    
 
 print(getUserPoint("Ann"))
