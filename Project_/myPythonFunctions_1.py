ec1c9115ab8186d6868e5958e25af528a37808fe
David Odebade <other.ode@gmail.com>
2020-07-20 18:39:08 +0100

    Sample Project Solutions

--- Project_/myPythonFunctions_1.py
+++ Project_/myPythonFunctions_1.py
@@ -4,7 +4,7 @@
 import random as r
 import os
 
-fileName = 'C:\\Users\\dev-research\\Documents\\Projects\\Applications\\Learn-Python-in-a-Day-\\Project_\\userScores1.txt'
+fileName = 'C:\\Users\\dev-research\\Documents\\Projects\\Applications\\Learn-Python-in-a-Day-\\Project_\\userScores.txt'
 
 # Exercise 2: Getting the User’s Score
 def getUserPoint(userName):
@@ -19,5 +19,13 @@ def getUserPoint(userName):
     except IOError as e:
         print('File not found ', e)
     
-
-print(getUserPoint("Ann"))
+# Exercise 3: Updating the User’s Score
+def updateUserPoints(newUser, userName, score):
+    if newUser:
+        f = open(fileName,'a')
+        f.append(userName, score)
+    elif not newUser:
+        f.open(fileName,'w')
+        for line in f:
+            name, score = line.split(",")
+            #if (name == userName)
